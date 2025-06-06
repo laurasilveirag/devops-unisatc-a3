3370ea00f1694e110b8c4233e54c6ebb8ea9e4c3
HenriqueSilva29 <henriqueforgiarini2909@gmail.com>
2025-06-05 23:26:59 -0300

    Testes automatizados end-2-end com o Playwright

---
+++ tests/create-author.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('cria novo autor', async ({ page }) => {
+  await page.goto('http://localhost:1337/admin/auth/login');
+  await page.getByRole('textbox', { name: 'Email' }).fill('admin@satc.edu.br');
+  await page.getByRole('textbox', { name: 'Password' }).fill('welcomeToStrapi123');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.getByRole('link', { name: 'Content Manager' }).click();
+  await page.getByRole('link', { name: 'Autor' }).click();
+  await page.locator('div').filter({ hasText: /^AutorCreate new entry$/ }).getByRole('link').click();
+
+  await page.getByRole('textbox', { name: 'Nome' }).fill('Laurinha');
+  await page.getByRole('textbox', { name: 'Email' }).fill('laura@gmail.com');
+  await page.getByRole('button', { name: 'Save' }).click();
+});
